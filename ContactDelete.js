b0b459174a8b973bb44fcc5b430b3b487924c510
thinkmiki <38305554+thinkmiki@users.noreply.github.com>
2022-12-09 00:16:37 -0500

    Add files via upload

---
+++ ContactDelete.js
@@ -0,0 +1,58 @@
+Platform.Load("Core", "1.1.1")
+
+var initiate = new InitiateDeleteProcess()
+
+function InitiateDeleteProcess() {
+	try {
+		/* -------- Intialize Data Extensions -------- */
+		var logDE = DataExtension.Init('dev_deletelog')
+		var queueDE = DataExtension.Init('dev_deletequeue')
+		var operationDE = DataExtension.Init('dev_deleteoperations')
+		var configDE = DataExtension.Init('dev_deleteconfig')
+
+		/* -------- Retrieve ConfigDE Values -------- */
+		var configSettings = configDE.Rows.Retrieve()[0]
+		var batchSize = parseInt(configSettings.batchSize)
+		var maxActive = parseInt(configSettings.maxActive)
+
+		/* -------- Check to see if ConfigDE contains values then calls the getToken() function if true -------- */
+
+		if (configSettings.length > 0) {
+			getToken(configDE, configSettings)
+		} else {
+			console.log("No Configuration Settings Found!")
+		}
+
+	} catch (error) {
+		return error
+	}
+}
+
+function getToken(configDE, configSettings) {
+	try {
+		/* -------- Set Auth Headers and Payload -------- */
+		var contentType = 'application/json'
+		var payload = Stringify({
+			"grant_type": "client_credentials",
+			"client_id": configSettings.client,
+			"client_secret": configSettings.secret
+		})
+
+		/* -------- POST Headers and Payload to generate token -------- */
+		var accessTokenResult = HTTP.Post(configSettings.authEndpoint + "/v2/token", contentType, payload)
+
+		/* -------- Retrieve Access Token and REST URL from response -------- */
+		var accessToken = Platform.Function.ParseJSON(accessTokenResult.Response[0]).access_token
+		var restBase = Platform.Function.ParseJSON(accessTokenResult.Response[0]).rest_instance_url
+
+		configDE.Rows.Update({
+				"authToken": accessToken,
+				"authRefreshDate": Now(),
+				"restEndpoint": restBase
+			},
+			["client"], [configSettings.client])
+
+	} catch (error) {
+		return error
+	}
+}
